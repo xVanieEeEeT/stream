059c78dc4df597d2d89460357dd02cae8d77b3d2
xVanieeeeeeeet <42171095+xVanieeeeeeeet@users.noreply.github.com>
2018-10-06 13:16:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,8 @@ const client = new Discord.Client();
 const prefix = '*';
 
 
-client.on('message', message => {
-    if(message.content.startsWith('st')) {
-        if(!message.author.id === '410778583682777098') return;
-        var arg = message.content.split(' ').slice(1).join(' ');
-        client.user.setGame(arg, "https://www.twitch.tv/9ivv");
+client.on('ready', async => {
+        client.user.setGame('- Paaain , xxx .', "https://www.twitch.tv/9ivv");
         console.log('Streaming.');
     }
 });
